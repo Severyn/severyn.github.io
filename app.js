96e10735265cbd728a5f2f609806ec49b139d489
Seweryn Michalewski <severyn.m@gmail.com>
2025-01-17 16:11:02 +0100

    change project

--- app.js
+++ app.js
@@ -1,28 +1,6 @@
-
-//swiper
-document.addEventListener('DOMContentLoaded', () => {
-    const swiper = new Swiper('.swiper', {
-        loop: true,
-        pagination: {
-            el: '.swiper-pagination',
-        },
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-        scrollbar: {
-            el: '.swiper-scrollbar',
-        },
-    });
-});
-
-
 document.addEventListener('DOMContentLoaded', function () {
     AOS.init({
         duration: 1000,
         once: true
     });
 });
-
-
-
